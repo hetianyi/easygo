e14fb27cd46d23fb88f640f140d5035d7d9c6727
hetianyi <hehety@outlook.com>
2022-03-06 16:40:59 +0800

    web app约定配置

--- httpx/mock_test.go
+++ httpx/mock_test.go
@@ -1,25 +1,104 @@
-package httpx_test
+package httpx
 
 import (
-	"encoding/json"
+	"fmt"
 	"github.com/hetianyi/easygo/file"
-	"github.com/hetianyi/easygo/httpx"
-	"io"
-	"io/ioutil"
-	"log"
-	"mime/multipart"
-	"net/http"
-	"path/filepath"
+	"github.com/hetianyi/easygo/logger"
+	jsoniter "github.com/json-iterator/go"
+	"reflect"
 	"testing"
-	"time"
 )
 
+func TestMock_RequestBody(t *testing.T) {
+	reader, _ := file.GetFile("C:\\Users\\Jason\\Downloads\\JetBrains 2022 最新版本全家桶激活.zip")
+	_ = []FormItem{
+		{
+			Type:  FormItemTypeField,
+			Name:  "userName",
+			Value: "张三",
+		},
+		{
+			Type:       FormItemTypeFile,
+			Name:       "userName",
+			FileName:   "张三",
+			FileReader: reader,
+		},
+	}
+	p := map[string][]string{
+		"": []string{"", ""},
+	}
+	Mock().RequestBody(p)
+}
+
+type ResultContainer struct {
+	Name string
+}
+
+func Test1(t *testing.T) {
+	var resp = &ResultContainer{}
+	logger.Info(checkResponseType(resp))
+	typ := reflect.TypeOf(resp)
+	logger.Info(typ)
+	logger.Info(typ.Kind() == reflect.Ptr)
+	logger.Info(typ.Elem())
+	for {
+		if typ.Kind() == reflect.Ptr {
+			typ = typ.Elem()
+			continue
+		}
+		break
+	}
+}
+
+func TestMock_Get(t *testing.T) {
+	/*var ret string
+	res, status, _ := Mock().Get("https://pti-ms-test.nuanyangjk.com/static/js/app.642a80de.js").
+		Success(&ret, 200).
+			ErrorCallback(func(status int, response []byte) {
+				logger.Fatal("请求错误，状态：", status, ": ", string(response))
+			}).
+		Do()
+	logger.Info(status)
+	logger.Info("请求返回：\n", res)*/
+
+	type BilibiliResponse struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		TTL     int    `json:"ttl"`
+		Data    struct {
+			GotoType  int    `json:"goto_type"`
+			GotoValue string `json:"goto_value"`
+			Id        int64  `json:"id"`
+			Name      string `json:"name"`
+			Seid      string `json:"seid"`
+			ShowName  string `json:"show_name"`
+			Type      int    `json:"type"`
+			Url       string `json:"url"`
+		} `json:"data"`
+	}
+	res, status, err := Mock().Get("https://api.bilibili.com/x/web-interface/search/default").
+		Success(&BilibiliResponse{}, 200).
+		Do()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Info(status)
+	marshal, _ := jsoniter.MarshalToString(res)
+	fmt.Println(marshal)
+	createFile, _ := file.CreateFile("D:/demo.txt")
+	createFile.WriteString(marshal)
+	createFile.Close()
+	logger.Info("请求返回：\n", marshal)
+	logger.Info("asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdas{dasdasdasdasdasdasdasdasdasd}asdasdas\"dasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd\"")
+}
+
+/*
 type Response struct {
 	Detail string `json:"detail"`
 }
 
 func TestMockGet1(t *testing.T) {
-	result, _, err := httpx.Mock().URL("https://hub.docker.com/v2/user/").Get().Success(&Response{}, 200, 401).Error(func(status int, response []byte) {
+	result, _, err := httpx.Mock().URL("https://hub.docker.com/v2/user/").Get().Success(&Response{}, 200, 401).ErrorCallback(func(status int, response []byte) {
 		log.Fatal("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -30,7 +109,7 @@ func TestMockGet1(t *testing.T) {
 }
 
 func TestMockGet2(t *testing.T) {
-	result, _, err := httpx.Mock().URL("https://hub.docker.com/v2/user/").Get().Success("", 200, 401).Error(func(status int, response []byte) {
+	result, _, err := httpx.Mock().URL("https://hub.docker.com/v2/user/").Get().Success("", 200, 401).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -50,7 +129,7 @@ type Response1 struct {
 }
 
 func TestMockGet3(t *testing.T) {
-	result, _, err := httpx.Mock().URL("https://hub.docker.com/api/content/v1/platforms").Get().Success(&Response1{}).Error(func(status int, response []byte) {
+	result, _, err := httpx.Mock().URL("https://hub.docker.com/api/content/v1/platforms").Get().Success(&Response1{}).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -65,7 +144,7 @@ func TestMockGet4(t *testing.T) {
 	result, _, err := httpx.Mock().URL("https://hub.docker.com/v2/repositories/library/redix/tags/").
 		Parameter("page_size", "25").
 		Parameter("page", "1").
-		Get().Success(new(map[string]interface{})).Error(func(status int, response []byte) {
+		Get().Success(new(map[string]interface{})).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -78,7 +157,7 @@ func TestMockGet4(t *testing.T) {
 
 func TestMockGet5(t *testing.T) {
 	result, _, err := httpx.Mock().URL("https://hub.docker.com/api/content/v1/products/images/redix").
-		Get().Success("").Error(func(status int, response []byte) {
+		Get().Success("").ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -93,7 +172,7 @@ func TestMockPost1(t *testing.T) {
 	result, _, err := httpx.Mock().URL("http://cdn.yifuls.com/api/cdn/detail").
 		Post().
 		Body(map[string][]string{"id": {"10004"}}).
-		Success(new(map[string]interface{})).Error(func(status int, response []byte) {
+		Success(new(map[string]interface{})).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -110,7 +189,7 @@ func TestMockPost2(t *testing.T) {
 	result, _, err := httpx.Mock().URL("http://cdn.yifuls.com/api/cdn/detail").
 		Post().
 		Body(map[string][]string{"id": {"10004"}}).
-		Success(nil).Error(func(status int, response []byte) {
+		Success(nil).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -131,7 +210,7 @@ func TestMockPost3(t *testing.T) {
 	log.Println("start ", time.Now().UTC())
 	result, _, err := httpx.Mock().URL("http://mobilepics.ws.126.net/UIg9y7iEZIrCxoikpo3HeNeikjLMqkV7%3D%3DFTRPUTL6.mp3").
 		Get().
-		Success(out).Error(func(status int, response []byte) {
+		Success(out).ErrorCallback(func(status int, response []byte) {
 		log.Println("status ", status, ", response: ", string(response))
 	}).Do()
 	if err != nil {
@@ -188,7 +267,7 @@ func TestMockUpload(t *testing.T) {
 	log.Println("start ", time.Now().UTC())
 	result, _, err := httpx.Mock().URL("http://localhost:8001/upload").
 		Success(nil).
-		Error(func(status int, response []byte) {
+		ErrorCallback(func(status int, response []byte) {
 			log.Println("status ", status, ", response: ", string(response))
 		}).
 		Multipart(func(writer *multipart.Writer) {
@@ -210,4 +289,4 @@ func TestMockUpload(t *testing.T) {
 		log.Println(string(bs))
 	}
 	log.Println("end ", time.Now().UTC())
-}
+}*/
